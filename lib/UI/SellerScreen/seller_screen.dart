b4f1f4b265cc7453461341101a0d78bf0326fc3a
Pranav M <pranavm265@gmail.com>
2025-01-11 17:07:36 +0530

    widgets created: placeholders

---
+++ lib/UI/SellerScreen/seller_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SellerScreen extends StatefulWidget {
+  const SellerScreen({super.key});
+
+  @override
+  State<SellerScreen> createState() => _SellerScreenState();
+}
+
+class _SellerScreenState extends State<SellerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
